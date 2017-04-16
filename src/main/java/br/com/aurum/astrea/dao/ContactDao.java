1b77878126ed8391866e89647100ec0f71c52595
Joao Paulo Rabelo <joaoprabelo@gmail.com>
2017-04-16 12:08:30 -0300

    dopost and doget for postman tests

--- src/main/java/br/com/aurum/astrea/dao/ContactDao.java
+++ src/main/java/br/com/aurum/astrea/dao/ContactDao.java
@@ -8,20 +8,19 @@ import com.googlecode.objectify.ObjectifyService;
 import br.com.aurum.astrea.domain.Contact;
 
 public class ContactDao {
-	
+
 	static {
 		ObjectifyService.register(Contact.class);
 	}
-	
+
 	public void save(Contact contact) {
-		// TODO: É preciso pesquisar como se usa o Objectify para armazenar a entidade contato no banco de dados.
+		ObjectifyService.ofy().save().entity(contact).now();
 	}
-	
+
 	public List<Contact> list() {
-		// TODO: É preciso pesquisar como se usa o Objectify para listar as entidades de contato.
-		return new ArrayList<>();
+		return ObjectifyService.ofy().load().type(Contact.class).list();
 	}
-	
+
 	public void delete(Long contactId) {
 		// TODO: É preciso pesquisar como se usa o Objectify para deletar entidades do banco de dados.
 	}
--- src/main/java/br/com/aurum/astrea/service/ContactServlet.java
+++ src/main/java/br/com/aurum/astrea/service/ContactServlet.java
@@ -10,25 +10,42 @@ import br.com.aurum.astrea.dao.ContactDao;
 
 @SuppressWarnings("serial")
 public class ContactServlet extends HttpServlet {
-	
+
 	private static final ContactDao DAO = new ContactDao();
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		// TODO: Implementar um método que irá ler o corpo da requisição e, com essas informações,
-		// salvar no banco de dados uma entidade do tipo 'Contato' com essas informações.
+		BufferedReader reader = req.getReader();
+		Gson gson = new Gson();
+
+		Contact c = gson.fromJson(reader, Contact.class);
+
+		DAO.save(c);
+
+		resp.setStatus(200);
+		resp.setContentType("application/json");
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		// TODO: Implementar um método que irá listar todas as entidades do tipo 'Contato' e devolver para o client essa listagem.
+		Gson gson = new Gson();
+		String id = req.getParameter("id");
+		String json;
+
+		if (id != null && !id.isEmpty()) {
+			json = gson.toJson(DAO.get(Long.parseLong(id)));
+		} else {
+			json = gson.toJson(DAO.list());
+		}
+
+		resp.setStatus(200);
+		resp.setContentType("application/json");
+
 	}
-	
+
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
+
 		// TODO: Implementar um método que irá deletar uma entidade do tipo 'Contato', dado parâmetro de identificação.
 	}
 }
